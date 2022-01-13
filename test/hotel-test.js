43d7a762c9318bd9d0ea15af7df5b2def1c3d142
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-12 23:00:19 -0800

    Write initial batch of tests for properties

--- test/hotel-test.js
+++ test/hotel-test.js
@@ -15,10 +15,27 @@ describe('Hotel', () => {
   let users;
 
   beforeEach(() => {
-    rooms = roomsTestData.map(room => new Room(room))
-    bookings = bookingData.map(booking => new Booking(booking))
-    users = customersTestData.map(user => new User(user))
-    hotel = new Hotel(hotel, rooms, bookings, users)
-    
-  })
+    rooms = roomsTestData.map(room => new Room(room));
+    bookings = bookingData.map(booking => new Booking(booking));
+    users = customersTestData.map(user => new User(user));
+    hotel = new Hotel(hotel, rooms, bookings, users);
+  });
+
+  it('should have a bookings array', () => {
+    expect(hotel.bookings).to.be.an('array');
+  });
+
+  it('should have an allRooms array', () => {
+    expect(hotel.allRooms).to.be.an('array');
+  });
+
+  it('should have an availableRooms array', () => {
+    expect(hotel.availableRooms).to.be.an('array');
+  });
+
+  it('should have an allUsers array', () => {
+    expect(hotel.allUsers).to.be.an('array')
+  });
+
+ 
 })
