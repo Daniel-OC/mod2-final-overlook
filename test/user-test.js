0db7122fc6f08593a815232183270ba6bec537e8
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-12 22:50:08 -0800

    Build initial user class and test

---
+++ test/user-test.js
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+import User from "../src/classes/User"
+import customersTestData from "../src/testing-data/Customers-test-data";
+import CustomersTestData from "../src/testing-data/Customers-test-data";
+
+describe('User', () => {
+
+  let user;
+  let users = customersTestData.map(user => user);
+
+  beforeEach(() => {
+    user = new User(users[0]);
+  });
+
+  it('should have an id number', () => {
+    expect(user.id).to.be.a('number');
+  });
+
+  it('should have a name', () => {
+    expect(user.name).to.be.a('string');
+  });
+
+  it('should have a bookings array', () => {
+    expect(user.bookings).to.be.an('array');
+  });
+
+});
