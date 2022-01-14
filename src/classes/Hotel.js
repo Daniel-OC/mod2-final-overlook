87da597d5ee9fa95e97bef02a75a7f7d7f90bab1
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-13 19:35:27 -0800

    Write updateAvailableRooms function

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -4,9 +4,21 @@ import Booking from './Booking'
 
 class Hotel{
   constructor(rooms, bookings, users) {
-    this.rooms = rooms.map(room => new Room(room));
+    this.allRooms = rooms.map(room => new Room(room));
     this.bookings = bookings.map(booking => new Booking(booking));
     this.allUsers = users.map(user => new User(user));
     this.availableRooms = []
   };
+
+  updateAvailableRooms(date) {
+   let todaysBookings = this.bookings.filter(booking => booking.date === date)
+   this.availableRooms = this.allRooms.reduce((acc, room) => {
+     if (!todaysBookings.find(booking => booking.roomNumber === room.number)) {
+       acc.push(room)
+     }
+     return acc
+   },[])
+  };
 }
+
+export default Hotel;
