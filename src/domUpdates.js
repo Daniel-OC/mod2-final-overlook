8d8aa9ee6ace576fb391b8ee219684bc7fd190bd
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-15 12:55:32 -0800

    Populate right display with selections based on date

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -5,6 +5,8 @@ import {
   startSite,
 } from './scripts'
 
+const dateInput = document.querySelector('#dateSelector');
+
 
 
 let domUpdates = {
@@ -37,8 +39,13 @@ let domUpdates = {
   },
   
   updateUserBookings(bookingDisplay) {
-    bookingDisplay.innerHTML = '<h3 class="self-center pink-font">Upcoming Bookings</h3>'
-    user.determineBookingCosts(hotel.allRooms)
+    user.determineBookingCosts(hotel.allRooms);
+    domUpdates.updateWithUpcomingBookings(bookingDisplay);
+    domUpdates.updateWithOldBookings(bookingDisplay);
+  },
+
+  updateWithUpcomingBookings(bookingDisplay) {
+    bookingDisplay.innerHTML = '<h3 class="self-center pink-font">Upcoming Bookings</h3>';
     user.upcomingBookings.forEach(booking => {
       bookingDisplay.innerHTML += `
     <section class="flex column around ">
@@ -46,6 +53,9 @@ let domUpdates = {
     <p>$${booking.price}</p>
   </section>`
     });
+  },
+
+  updateWithOldBookings(bookingDisplay) {
     bookingDisplay.innerHTML += '<h3 class="self-center pink-font">Past Bookings</h3>'
     user.pastBookings.forEach(booking => {
       bookingDisplay.innerHTML += `
@@ -60,28 +70,36 @@ let domUpdates = {
   //to do tomorrow to function below:
   
   updateRightDisplay() {
-    let dateInput = document.querySelector('#dateSelector')
+    console.log(dateSelector, dateSelector.value)
     let availableRooms = document.querySelector('#bottomRightSection')
-    if (dateInput.value) {
-      hotel.updateAvailableRooms(dateInput)
-    }
+    availableRooms.innerHTML = ''
+    hotel.updateAvailableRooms(dateSelector.value.replaceAll("-","/"))
     hotel.availableRooms.forEach(room => {
       availableRooms.innerHTML += `
       <section id="roomCard" class="flex row between full-width">
             <section>
-              <p class="margin-none">Room ${room.roomNumber}</p>
-              <p class="font-xxl margin-none">15</p>
+              <p class="margin-none">Room</p>
+              <p class="font-xxl margin-none">${room.number}</p>
             </section>
             <section class="flex row between">
               <section class="flex column">
-                <p class="sml-mrgn-btm margin-none">${room.roomType}</p>
-                <p class="sml-mrgn-btm margin-none">Beds:${room.beds} ${room.bedSize}</p>
-                <p class="sml-mrgn-btm margin-none">Bidet: ${room.bidet}</p>
+                <p class="sml-mrgn-btm margin-none">${room.roomType.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}</p>
+                <p class="sml-mrgn-btm margin-none">Beds: ${room.numBeds} ${room.bedSize.charAt(0).toUpperCase() + room.bedSize.slice(1) }</p>
+                <p class="sml-mrgn-btm margin-none">Bidet: ${room.bidet ? 'Yes' : 'No'}</p>
               </section>
-            </section>`
+            </section>
+            <section class="flex column center">
+              <button>$450</button>
+              <p>BOOK NOW</p>
+            </section>
+        </section>`
     })
   }
 }
+
+//Event Listeners
+dateInput.addEventListener('change', domUpdates.updateRightDisplay)
+
 
 export {
   domUpdates,
