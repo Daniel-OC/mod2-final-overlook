b53fdb1c4b159049b34be278d5e9f003066c07d7
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-15 00:27:03 -0800

    Transfer cost methods over from Hotel, and start to build past and future bookings functionality

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,12 +1,38 @@
+import { hotel } from "../scripts";
 import Hotel from "./Hotel";
 
 class User{
   constructor(user) {
     this.id = user.id,
     this.name = user.name,
-    this.bookings = []
+    this.allBookings = [],
+    this.pastBookings = [],
+    this.upcomingBookings = []
   }
 
+  determineBookingCosts(hotelRooms) {
+    this.allBookings.map(booking => {
+      hotelRooms.forEach(room => {
+        if (room.number === booking.roomNumber) {
+          booking.price = room.costPerNight
+        }
+      })
+      
+    })
+  }
+
+  calculateTotalCosts() {
+    return parseFloat(this.allBookings.reduce((acc, booking) => {
+      acc+= booking.price
+      return acc
+    }, 0)).toFixed(2)
+  }
+
+  divideBookingsByDate() {
+    let currentDate = new Date().toISOString().substr(0,10).replaceAll("-","/")
+    console.log(currentDate)
+
+  }
 }
 
 export default User;
