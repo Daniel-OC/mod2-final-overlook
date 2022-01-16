c7eefdc98504b1773c50a5c07d2cf890d02557d1
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-01-16 15:00:20 -0800

    Update Scripts for updated api calls

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import {getAllUsers, getAllRooms, getAllBookings, addNewBooking, deleteBooking} from './apiCalls'
+import {getInitialUsers, getInitialRooms, getInitialBookings, addNewBooking, deleteBooking, getAllBookings} from './apiCalls'
 import {
   domUpdates,
   
@@ -16,10 +16,10 @@ import Booking from './classes/Booking';
 import User from './classes/User';
 import Hotel from './classes/Hotel';
 
-let user
-let hotel;
+var user
+var hotel;
 
-Promise.all([getAllRooms, getAllBookings, getAllUsers])
+Promise.all([getInitialRooms, getInitialBookings, getInitialUsers])
   .then(data => {
     createHotel(data);
     startSite();
@@ -32,27 +32,28 @@ const createHotel = (data) => {
 
 const startSite = () => {
   user = new User (hotel.allUsers[0]);
-  user.allBookings = hotel.findUsersBookings(user.id)
-  user.divideBookingsByDate()
-  console.log(user.allBookings)
-  console.log(user)
-  domUpdates.updateLeftDisplay(user);
+  updateSite()
 };
 
-
+const updateSite = () => {
+  console.log(user)
+  user.allBookings = hotel.findUsersBookings(user.id)
+  user.divideBookingsByDate()
+  domUpdates.updateLeftDisplay(user);
+}
 
 const updateBookings = (data) => {
-  console.log(hotel.bookings)
-  hotel.bookings = data.bookings.map(booking => booking)
-  console.log(hotel.bookings)
-}
+  hotel.bookings = data.bookings;
+};
 
-async function sendBookingToApi(date, roomNumber) { 
-  let booking = user.createBookingObject(date, roomNumber);
-  await addNewBooking(booking)
-  await getAllBookings.then(data => updateBookings(data))
-  await startSite()
-}
+const sendBookingToApi = (date, roomNumber) => {
+  const booking = user.createBookingObject(date, roomNumber);
+  addNewBooking(booking).then(getAllBookings).then(data => {
+    updateBookings(data);
+    updateSite();
+  });
+};
+
 
 
 export {
